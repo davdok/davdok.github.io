361d877aa394cfc47759e0e9bc09b6eb873d7a30
davdok <dokesdavid@gmail.com>
2016-04-03 14:31:58 +0200

    First Commit WIP

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Fullscreen background
+    */
+    /*$.backstretch("assets/img/backgrounds/mac.jpg");*/
+
+    $.backstretch([
+        "assets/img/backgrounds/beach.jpg"
+      , "assets/img/backgrounds/beach2.jpg"
+      , "assets/img/backgrounds/desk.jpg"
+      ,"assets/img/backgrounds/mac.jpg"
+      ,"assets/img/backgrounds/miami.jpg"
+      ,"assets/img/backgrounds/notebook.jpg"
+      ,"assets/img/backgrounds/sea.jpg"
+      ,"assets/img/backgrounds/table.jpg"
+      ,"assets/img/backgrounds/van.jpg"
+      ,"assets/img/backgrounds/work.jpg"
+      ,"assets/img/backgrounds/work3.jpg"
+    ], {duration: 5000,fade: 750});
+
+
+});
